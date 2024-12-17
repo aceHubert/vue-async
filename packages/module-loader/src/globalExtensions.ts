28910ce2e3b07d466c3d945d0229f4b78fed2fa1
hubert <hubert@lejian.com>
2024-12-17 10:51:37 +0800

    chore(@vue-async/module-loader): types export

--- packages/module-loader/src/globalExtensions.ts
+++ packages/module-loader/src/globalExtensions.ts
@@ -15,6 +15,17 @@ declare module 'vue/types/vue' {
   }
 }
 
+// @ts-ignore: works on Vue 2, fails in Vue 3
+declare module 'vue/types/options' {
+  interface ComponentOptions<V> {
+    /**
+     * module loader  instance to install in your application. Should be passed to the
+     * root Vue.
+     */
+    moduleLoader?: ModuleLoader;
+  }
+}
+
 // @ts-ignore: works on Vue 3, fails in Vue 2
 declare module '@vue/runtime-core' {
   export interface ComponentCustomProperties {
